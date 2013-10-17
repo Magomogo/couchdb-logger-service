2b1c24cb0b00eafc1bdba9f58cb6e47dcc308f86
Magomogo <mgnatenko@gmail.com>
2013-10-17 11:07:54 +0700

    separated templates-related logic

---
+++ www/js/controller.js
@@ -0,0 +1,55 @@
+(function (window, $, History, renderer) {
+    "use strict";
+
+    var rowsPerPage = 20, currentPage = 0;
+
+    History.Adapter.bind(window, 'statechange', function () {
+        var state = History.getState();
+
+        if (state.data.page) {
+            navigate(state.data.page, state.data.param, 'do not store history change');
+        }
+    });
+
+    function navigate(pageName, param, doNotStoreHistoryChange) {
+        var actions = {
+            page: function (num) {
+                currentPage = num;
+
+                $.getJSON('all/' + num * rowsPerPage + '/' + rowsPerPage, function (data) {
+                    $('#view').html(renderer.renderList(data));
+                });
+
+                return 'Page ' + (num + 1);
+            },
+            record: function (id) {
+
+                $.getJSON('record/' + id, function (data) {
+                    $('#view').html(renderer.renderRecord(data, currentPage));
+                });
+
+                return 'Record ' + id;
+            }
+        }, pageTitle;
+
+        pageTitle = actions[pageName](param);
+
+        if (!doNotStoreHistoryChange) {
+            History.pushState({page: pageName, param: param}, pageTitle, '');
+        }
+
+    }
+
+    window.logger = {
+
+        init: function () {
+            renderer.loadTemplates(function () {
+                navigate('page', 0, 'do not store history change');
+                History.replaceState({page: 'page', param: 0}, 'Page 1', '')
+            });
+        },
+
+        navigate: navigate
+    }
+
+}(window, jQuery, History, renderer));
---
+++ www/js/renderer.js
@@ -0,0 +1,95 @@
+(function (window, $, moment, mustache) {
+    "use strict";
+
+    var templates = {}, rowsPerPage = 20;
+
+    function pageNavigationHelper () {
+        return function (content, render) {
+
+            var pageNum, nav = [], navLinksLimit = 5, data = this;
+
+            for (pageNum = Math.max(0, parseInt(data.offset / rowsPerPage) - 2);
+                 pageNum < parseInt(data.total_rows / rowsPerPage) && navLinksLimit--;
+                 pageNum++) {
+
+                nav.push({title: pageNum + 1, num: pageNum, active: pageNum === parseInt(data.offset / rowsPerPage)});
+            }
+
+            this.isFirst = data.offset < rowsPerPage;
+            this.prev = parseInt(data.offset / rowsPerPage) - 1;
+            this.nav = nav;
+            this.next = parseInt(data.offset / rowsPerPage) + 1;
+            this.isLast = data.offset >= data.total_rows - rowsPerPage;
+
+            return render(content);
+        }
+    }
+
+    function dateTimeHelper (format, includeFromNow) {
+        return function () {
+            return function (content, render) {
+                var date = moment(render(content));
+                return date.format(format) + (includeFromNow ? ', ' + date.fromNow() : '');
+            }
+        }
+    }
+
+    function printJsonAsHtmlHelper () {
+        return function () {
+            var html = '<dl>', key, json = this;
+
+            for (key in json) {
+                if (json.hasOwnProperty(key) && (['_id', '_rev', 'timestamp', 'message', 'channel'].indexOf(key) === -1)) {
+                    if (typeof json[key] == 'object') {
+                        html += printJsonAsHtmlHelper(json[key]);
+                    } else {
+                        html += '<dt>' + key + '</dt>'
+                        html += '<dd>' + json[key] + '</dd>'
+                    }
+                }
+            }
+            return html + '</dl>';
+        }
+    }
+
+    function registerHelpers (view) {
+        view.h_dateTimeList = dateTimeHelper("D.MM.YYYY HH:mm", 'fromNow');
+        view.h_dateTimeRecord = dateTimeHelper("D MMM YYYY HH:mm:ss.SSS");
+        view.h_pageNav = pageNavigationHelper;
+        view.h_prettyJson = printJsonAsHtmlHelper;
+
+        return view;
+    }
+
+    window.renderer = {
+
+        loadTemplates: function (done) {
+
+            $.when(
+                $.get('mustache/list.mustache', function (template) {
+                    templates.list = mustache.compile(template);
+                }),
+                $.get('mustache/record.mustache', function (template) {
+                    templates.record = mustache.compile(template);
+                }),
+                $.get('mustache/paginator.mustache', function (template) {
+                    mustache.compilePartial('paginator', template);
+                }),
+                $.get('mustache/page-nav.mustache', function (template) {
+                    mustache.compilePartial('nav', template);
+                })
+            ).then(done);
+
+        },
+
+        renderList: function (view) {
+            return templates.list(registerHelpers(view));
+        },
+
+        renderRecord: function (record, currentPage) {
+            return templates.record(registerHelpers({currentPage: currentPage, record: record}));
+        }
+
+    }
+
+}(window, jQuery, moment, Mustache));
